15c274e9c0ab550db732d4619869185762f4098e
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-21 09:02:19 -0300

    Updating .py file

--- arrays_numpy.py
+++ arrays_numpy.py
@@ -91,27 +91,10 @@ import numpy as np
 # print(a[1, 2])      #[linha, coluna]
 # print(a[0:2, 2])
 
-a = np.array([[1, 2, 3], [4, 5, 6]], dtype = float)
+# a = np.array([[1, 2, 3], [4, 5, 6]], dtype = float)
+# print(a)
+
+a = np.array([[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]]])
 print(a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(a.shape)      
+print(a.ndim)
