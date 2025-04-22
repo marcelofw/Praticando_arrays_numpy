a722edafd016877ba8d46df33ba099d370e5332e
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-22 10:11:59 -0300

    adding .py file

---
+++ estatistica_numpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array([18, 22, 44, 29, 39, 58])
+media = np.mean(a)
+print(media)
+
+variancia = np.var(a)
+print(variancia)
+
+desvio_padrao = np.std(a)
+print(desvio_padrao)
